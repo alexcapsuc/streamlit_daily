da4f57d85185a7dd83a95991244e9214ec5459ae
acapsuc <acapsuc@gmail.com>
2025-08-08 14:51:10 +1000

    changed query to predefined numbers

--- queries/overview_kpi.sql
+++ queries/overview_kpi.sql
@@ -1,10 +1,6 @@
 SELECT
-    SUM(deposit_amount) AS TOTAL_DEPOSITS,
-    SUM(withdrawal_amount) AS TOTAL_WITHDRAWALS,
-    SUM(trading_volume) AS TOTAL_VOLUME,
-    SUM(profit) AS TOTAL_PROFIT,
-    IFF(NULLIF(SUM(trading_volume),0) IS NULL, 0, SUM(profit)/SUM(trading_volume)) AS MARGIN
-FROM trading_summary
-WHERE date BETWEEN '{start}' AND '{end}'
-  AND asset_id IN ({asset_ids})
-  AND duration_id IN ({duration_ids});
+    100000 AS TOTAL_DEPOSITS,
+    90000 AS TOTAL_WITHDRAWALS,
+    1000000 AS TOTAL_VOLUME,
+    50000 AS TOTAL_PROFIT,
+    TOTAL_PROFIT / TOTAL_VOLUME AS MARGIN
